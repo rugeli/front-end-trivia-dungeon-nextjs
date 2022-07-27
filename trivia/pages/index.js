5d2166273df87f87f3282dbed3d096278bc5ae47
Ruge Li <rugeli0605@gmail.com>
2022-07-27 15:44:10 -0700

    added a layout component

--- trivia/pages/index.js
+++ trivia/pages/index.js
@@ -7,13 +7,11 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>This is the homepage</p>
       <Link href="/aboutApp">
         <a>See the planning story</a>
       </Link>
-      <Footer />
     </div>
   )
 }
