5fe431d366213ea70bd274c5123556b232037384
Ruge Li <rugeli0605@gmail.com>
2022-07-27 15:31:53 -0700

    added an about app folder, linked up between pages

--- trivia/components/Navbar.js
+++ trivia/components/Navbar.js
@@ -1,13 +1,15 @@
+import Link from 'next/link'
+
 const Navbar = () => {
   return ( 
     <nav>
       <div className="logo">
         <h1>Trivia Dungeon</h1>
       </div>
-      <a>Home</a>
-      <a>Play</a>
-      <a>Leaderboard</a>
-      <a>LogIn/SignUp</a>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/quiz"><a>Play</a></Link>
+      <Link href="/leaderboard"><a>Leaderboard</a></Link>
+      <Link href="/authentication"><a>LogIn/SignUp</a></Link>
 
     </nav>
   );
--- trivia/pages/index.js
+++ trivia/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
       <Navbar />
       <h1>Homepage</h1>
       <p>This is the homepage</p>
+      <Link href="/aboutApp">
+        <a>See the planning story</a>
+      </Link>
       <Footer />
     </div>
   )
