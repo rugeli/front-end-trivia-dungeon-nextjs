cce1c9d2cd133d15a0a7f09bb5748cb388c00174
Ruge Li <rugeli0605@gmail.com>
2022-07-27 14:59:53 -0700

    added pages, footer and navbar

---
+++ trivia/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return ( 
+    <div>
+      Made by Ruge
+    </div>
+  );
+}
+
+export default Footer;
---
+++ trivia/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return ( 
+    <nav>
+      <div className="logo">
+        <h1>Trivia Dungeon</h1>
+      </div>
+      <a>Home</a>
+      <a>Play</a>
+      <a>Leaderboard</a>
+      <a>LogIn/SignUp</a>
+
+    </nav>
+  );
+}
+
+export default Navbar;
