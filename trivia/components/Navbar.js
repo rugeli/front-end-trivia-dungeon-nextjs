3a8ee2556b8c0f506de90211daedeb00a8c08c9f
Ruge Li <rugeli0605@gmail.com>
2022-07-28 20:49:23 -0700

    created a 404 comp, moved new game out of pages file

--- trivia/components/Navbar.js
+++ trivia/components/Navbar.js
@@ -4,13 +4,13 @@ const Navbar = () => {
   return ( 
     <nav>
       <div className="logo">
-        <h1>Trivia Dungeon</h1>
+        <h1>
+          <Link href="/"><a>Trivia Dungeon</a></Link>
+        </h1>
       </div>
-      <Link href="/"><a>Home</a></Link>
-      <Link href="/quiz"><a>Play</a></Link>
+      <Link href="/planning"><a>Planning Story</a></Link>
       <Link href="/leaderboard"><a>Leaderboard</a></Link>
       <Link href="/authentication"><a>LogIn/SignUp</a></Link>
-
     </nav>
   );
 }
--- trivia/pages/index.js
+++ trivia/pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
 import Link from 'next/link'
 
 export default function Home() {
@@ -9,8 +7,8 @@ export default function Home() {
     <div> 
       <h1 className={styles.title}>Homepage</h1>
       <p className={styles.text}>This is the homepage</p>
-      <Link href="/aboutApp">
-        <a className={styles.btn}>Planning Story</a>
+      <Link href="/newGame">
+        <a className={styles.btn}>New Game</a>
       </Link>
     </div>
   )
